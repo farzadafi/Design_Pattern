10222ff11d877c4035be370da6621d3c48b1c7d6
farzadafi <farzadafi40@gmail.com>
2023-02-01 16:24:39 +0330

    add parse method

--- Abstract_Class/src/main/java/ExtractorRepost.java
+++ Abstract_Class/src/main/java/ExtractorRepost.java
@@ -1,7 +1,32 @@
+import com.google.common.base.Strings;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class ExtractorRepost {
 
     public abstract Pattern getPattern();
     public abstract String getReportName();
+
+    private String parse(String path) throws FileNotFoundException {
+        StringBuilder out = new StringBuilder();
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        if(scanner.hasNext())
+            scanner.nextLine();
+        else
+            return "Empty file";
+        while ( scanner.hasNext()) {
+            String nextLine = scanner.nextLine();
+            Matcher matcher = getPattern().matcher(nextLine);
+            boolean matches = matcher.matches();
+            if(matches) {
+                out.append(nextLine).append("/n");
+            }
+        }
+        return Strings.isNullOrEmpty(out.toString()) ? "Empty file" : out.toString();
+    }
 }
