ef8e2cf5383aedd4eab7e16807778ed775a65237
farzadafi <farzadafi40@gmail.com>
2023-02-01 17:01:45 +0330

    add one text for NumberExtractorReport class

--- Abstract_Class/src/main/java/Main.java
+++ Abstract_Class/src/main/java/Main.java
@@ -1,2 +1,7 @@
+import java.io.FileNotFoundException;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        new NumberExtractorReport().prepareAndSendReport("data.txt");
+    }
 }
