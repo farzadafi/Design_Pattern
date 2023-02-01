b40db184d2aa32ffb6db0a8459d2d4b338384b73
farzadafi <farzadafi40@gmail.com>
2023-02-01 17:05:46 +0330

    add one test for EmailExtractorReport class

--- Abstract_Class/src/main/java/Main.java
+++ Abstract_Class/src/main/java/Main.java
@@ -3,5 +3,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         new NumberExtractorReport().prepareAndSendReport("data.txt");
+        new EmailExtractorReport().prepareAndSendReport("data.txt");
     }
 }
