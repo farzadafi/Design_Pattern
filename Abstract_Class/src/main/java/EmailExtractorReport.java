b28db71e02fb87c47f79132f9dec0463f599aa5e
farzadafi <farzadafi40@gmail.com>
2023-02-01 17:03:08 +0330

    extend from ExtractorReport class and override two method

--- Abstract_Class/src/main/java/EmailExtractorReport.java
+++ Abstract_Class/src/main/java/EmailExtractorReport.java
@@ -1,2 +1,14 @@
-public class EmailExtractorReport {
+import java.util.regex.Pattern;
+
+public class EmailExtractorReport extends ExtractorReport {
+
+    @Override
+    public Pattern getPattern() {
+        return null;
+    }
+
+    @Override
+    public String getReportName() {
+        return null;
+    }
 }
