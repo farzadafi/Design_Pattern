54d99f2dcb5fa146219b870d05b4eea65daecc0e
farzadafi <farzadafi40@gmail.com>
2023-02-01 17:05:25 +0330

    add pattern and override body methods

--- Abstract_Class/src/main/java/EmailExtractorReport.java
+++ Abstract_Class/src/main/java/EmailExtractorReport.java
@@ -2,13 +2,17 @@ import java.util.regex.Pattern;
 
 public class EmailExtractorReport extends ExtractorReport {
 
+    private static final Pattern PATTERN =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+
     @Override
     public Pattern getPattern() {
-        return null;
+        return PATTERN;
     }
 
     @Override
     public String getReportName() {
-        return null;
+        return "Email";
     }
 }
