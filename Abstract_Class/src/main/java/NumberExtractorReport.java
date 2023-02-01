c6a1371067e1100811517e680d701b42b704a257
farzadafi <farzadafi40@gmail.com>
2023-02-01 16:29:47 +0330

    override body of getPattern and getReportName methods

--- Abstract_Class/src/main/java/NumberExtractorReport.java
+++ Abstract_Class/src/main/java/NumberExtractorReport.java
@@ -1,11 +1,14 @@
 import java.util.regex.Pattern;
 
 public class NumberExtractorReport extends ExtractorReport {
+
+    private static final Pattern PATTERN = Pattern.compile("^[0-9]*$");
+
     public Pattern getPattern() {
-        return null;
+        return PATTERN;
     }
 
     public String getReportName() {
-        return null;
+        return "Phone Number";
     }
 }
