5694d09a5da984089a9e191ef38b37e11c99986c
farzadafi <farzadafi40@gmail.com>
2023-02-01 20:07:09 +0330

    add some field and two incorrect method

--- Single_Responsibility_Principle/src/main/java/Employee.java
+++ Single_Responsibility_Principle/src/main/java/Employee.java
@@ -1,2 +1,18 @@
+import java.util.Date;
+
 public class Employee {
+
+    private String id;
+    private String name;
+    private String address;
+    private Date dateOfJoining;
+
+    // this two methods here is wrong !!!
+//    public boolean isPromotionDueThisYear() {
+//        // promotion logic
+//    }
+
+//    public double calcIncomeTaxForCurrentYear() {
+//        //income tax logic
+//    }
 }
