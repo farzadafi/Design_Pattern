1f2080e223e762cdaaf1bf9232d4f40e36b576c5
farzadafi <farzadafi40@gmail.com>
2023-02-01 16:26:33 +0330

    add prepareAndSendReport method

--- Abstract_Class/src/main/java/ExtractorRepost.java
+++ Abstract_Class/src/main/java/ExtractorRepost.java
@@ -9,24 +9,32 @@ import java.util.regex.Pattern;
 public abstract class ExtractorRepost {
 
     public abstract Pattern getPattern();
+
     public abstract String getReportName();
 
     private String parse(String path) throws FileNotFoundException {
         StringBuilder out = new StringBuilder();
         File file = new File(path);
         Scanner scanner = new Scanner(file);
-        if(scanner.hasNext())
+        if (scanner.hasNext())
             scanner.nextLine();
         else
             return "Empty file";
-        while ( scanner.hasNext()) {
+        while (scanner.hasNext()) {
             String nextLine = scanner.nextLine();
             Matcher matcher = getPattern().matcher(nextLine);
             boolean matches = matcher.matches();
-            if(matches) {
+            if (matches) {
                 out.append(nextLine).append("/n");
             }
         }
         return Strings.isNullOrEmpty(out.toString()) ? "Empty file" : out.toString();
+    }
+
+    public void prepareAndSendReport(String path) throws FileNotFoundException {
+        System.out.println("Starting report " + getReportName());
+        String report = parse(path);
+        System.out.println(report);
+        System.out.println("Send Report " + getReportName());
     }
 }
