886ce8b8d1bd6e7bcb65c37bb1a27c6ef01e70a9
farzadafi <farzadafi40@gmail.com>
2023-02-01 19:58:46 +0330

    add isPromotionDueThisYear method

--- Single_Responsibility_Principle/src/main/java/HRPromotions.java
+++ Single_Responsibility_Principle/src/main/java/HRPromotions.java
@@ -1,3 +1,7 @@
 public class HRPromotions {
 
+    public boolean isPromotionDueThisYear(Employee employee) {
+        //promotion logic implementation is using the employee information passed in
+        return true;
+    }
 }
