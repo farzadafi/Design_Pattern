7bf736c38b77d82faa46b2d57133d6d500ec62ca
farzadafi <farzadafi40@gmail.com>
2023-02-01 16:28:37 +0330

    extend from ExtractorReport and override two method

--- Abstract_Class/src/main/java/NumberExtractorReport.java
+++ Abstract_Class/src/main/java/NumberExtractorReport.java
@@ -1,2 +1,11 @@
-public class NumberExtractorReport {
+import java.util.regex.Pattern;
+
+public class NumberExtractorReport extends ExtractorReport {
+    public Pattern getPattern() {
+        return null;
+    }
+
+    public String getReportName() {
+        return null;
+    }
 }
