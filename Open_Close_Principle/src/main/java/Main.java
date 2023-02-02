dc3ecba32ef1665b5d89d2ac23f2d1b35696cec1
farzadafi <farzadafi40@gmail.com>
2023-02-02 11:18:15 +0330

    insert all code

--- Open_Close_Principle/src/main/java/Main.java
+++ Open_Close_Principle/src/main/java/Main.java
@@ -1,2 +1,36 @@
-public class Main {
+public interface Shape {
+    double calculateArea();
+}
+
+class Rectangle implements Shape {
+    public double length;
+    public double width;
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Circle implements Shape{
+    public double radius;
+
+    @Override
+    public double calculateArea() {
+        return (22/7) * radius * radius;
+    }
+}
+
+class AreaCalculator {
+    public double calculateShapeArea(Shape shape) {
+        return shape.calculateArea();
+    }
+
+//    public double calculateRectangleArea(Rectangle rectangle) {
+//        return rectangle.length * rectangle.width;
+//    }
+//
+//    public double calculateCircleArea(Circle circle) {
+//        return (22 / 7) * circle.radius * circle.radius;
+//    }
 }
