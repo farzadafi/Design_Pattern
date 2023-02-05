6b8b072bbe2ac622a0264e69d892b6858d531fa6
farzadafi <farzadafi40@gmail.com>
2023-02-06 03:23:05 +0330

    create RestaurantInterface and add five need method

---
+++ InterfaceSegregationResturant/src/main/java/RestaurantInterface.java
@@ -0,0 +1,8 @@
+public interface RestaurantInterface {
+    void acceptOnlineOrder();
+    void takeTelephoneOrder();
+    void payOnline();
+    void walkInCustomerOrder();
+    void payInPerson();
+}
+
