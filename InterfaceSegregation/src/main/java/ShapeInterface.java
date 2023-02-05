d90ce24d9ff8a31a0f03da264cc134c27f8e0b31
farzadafi <farzadafi40@gmail.com>
2023-02-06 03:17:48 +0330

    add new two interface and two class

--- InterfaceSegregation/src/main/java/ShapeInterface.java
+++ InterfaceSegregation/src/main/java/ShapeInterface.java
@@ -1,2 +1,42 @@
 public interface ShapeInterface {
+    double area();
+}
+
+interface SolidShapeInterface {
+    double volume();
+}
+
+interface ManageShapeInterface {
+    double calculate();
+}
+
+class Cube implements ShapeInterface, SolidShapeInterface, ManageShapeInterface {
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area() + this.volume();
+    }
+}
+
+class Square implements ShapeInterface, ManageShapeInterface {
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area();
+    }
 }
