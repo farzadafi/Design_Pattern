ca9b2c29e0857748ab68334e641d27d6be300851
farzadafi <farzadafi40@gmail.com>
2023-02-06 03:23:47 +0330

    create and implements RestaurantInterface and override five method

---
+++ InterfaceSegregationResturant/src/main/java/OnlineClientImpel.java
@@ -0,0 +1,27 @@
+public class OnlineClientImpel implements RestaurantInterface {
+
+    @Override
+    public void acceptOnlineOrder() {
+
+    }
+
+    @Override
+    public void takeTelephoneOrder() {
+
+    }
+
+    @Override
+    public void payOnline() {
+
+    }
+
+    @Override
+    public void walkInCustomerOrder() {
+
+    }
+
+    @Override
+    public void payInPerson() {
+
+    }
+}
