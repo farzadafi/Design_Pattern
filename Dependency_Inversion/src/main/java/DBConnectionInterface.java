d506036fb720cf7861b3de4d58882af61c85f585
farzadafi <farzadafi40@gmail.com>
2023-02-06 14:25:44 +0330

    create two new class

--- Dependency_Inversion/src/main/java/DBConnectionInterface.java
+++ Dependency_Inversion/src/main/java/DBConnectionInterface.java
@@ -1,2 +1,19 @@
 public interface DBConnectionInterface {
+    int connect();
+}
+
+class MySqlConnection implements DBConnectionInterface {
+
+    @Override
+    public int connect() {
+        return 0;
+    }
+}
+
+class PasswordReminder {
+    private int dbConnection;
+
+    public PasswordReminder(MySqlConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
 }
