553a76077b8a9dfc87ab599f3c8ca21ea773de2e
farzadafi <farzadafi40@gmail.com>
2023-02-13 11:08:24 +0330

    add getShape method

--- FactoryConcreateCreator/src/main/java/ShapeFactory.java
+++ FactoryConcreateCreator/src/main/java/ShapeFactory.java
@@ -1,2 +1,15 @@
 public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null)
+            return null;
+        else if (shapeType.equalsIgnoreCase("CIRCLE"))
+            return new Circle();
+        else if (shapeType.equalsIgnoreCase("RECTANGLE"))
+            return new Rectangle();
+        else if (shapeType.equalsIgnoreCase("SQUARE"))
+            return new Circle();
+
+        return null;
+    }
 }
