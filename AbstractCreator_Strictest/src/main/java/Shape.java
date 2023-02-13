bb61fe718c74b7f5f046563502b89031497b4735
farzadafi <farzadafi40@gmail.com>
2023-02-13 06:03:31 +0330

    add Square class and implements shape interface and override draw method

--- AbstractCreator_Strictest/src/main/java/Shape.java
+++ AbstractCreator_Strictest/src/main/java/Shape.java
@@ -18,3 +18,12 @@ class Circle implements Shape {
     }
 }
 
+class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+
