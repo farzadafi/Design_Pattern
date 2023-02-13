21315facfef77f4c48a93073aeee97f7559fd215
farzadafi <farzadafi40@gmail.com>
2023-02-13 06:10:18 +0330

    add main method

--- AbstractCreator_Strictest/src/main/java/Client.java
+++ AbstractCreator_Strictest/src/main/java/Client.java
@@ -1,2 +1,13 @@
 public class Client {
+    public static void main(String[] args) {
+        Shape shape1 = new CircleFactory().getShape();
+        shape1.draw();
+
+        Shape shape2 = new RectangleFactory().getShape();
+        shape2.draw();
+
+        Shape shape3 = new SquareFactory().getShape();
+        shape3.draw();
+
+    }
 }
