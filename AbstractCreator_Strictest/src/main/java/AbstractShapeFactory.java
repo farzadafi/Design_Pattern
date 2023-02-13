5c530924f9fbba08df331f91c5a93b85305f96b9
farzadafi <farzadafi40@gmail.com>
2023-02-13 06:05:42 +0330

    add getShape method

--- AbstractCreator_Strictest/src/main/java/AbstractShapeFactory.java
+++ AbstractCreator_Strictest/src/main/java/AbstractShapeFactory.java
@@ -1,2 +1,8 @@
-public class AbstractShapeFactory {
+public abstract class AbstractShapeFactory {
+
+    protected abstract Shape factoryMethod();
+
+    public Shape getShape() {
+        return factoryMethod();
+    }
 }
