4901f303a676dbc9a288851b3315d22adee50f7d
farzadafi <farzadafi40@gmail.com>
2023-02-14 06:53:53 +0330

    add main method

--- Factory_Method/StaticMethodCreatro/src/main/java/Client.java
+++ Factory_Method/StaticMethodCreatro/src/main/java/Client.java
@@ -1,3 +1,12 @@
 public class Client {
+    public static void main(String[] args) {
+        Shape shape1 = ShapeFactory.getShape("CIRCLE");
+        shape1.draw();
 
+        Shape shape2 = ShapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        Shape shape3 = ShapeFactory.getShape("SQUARE");
+        shape3.draw();
+    }
 }
