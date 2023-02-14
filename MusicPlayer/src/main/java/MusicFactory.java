5e3387d23794cadde5e5bd2c60f4f34856f3a780
farzadafi <farzadafi40@gmail.com>
2023-02-14 06:25:31 +0330

    add getMusicPlayer method

--- MusicPlayer/src/main/java/MusicFactory.java
+++ MusicPlayer/src/main/java/MusicFactory.java
@@ -1,2 +1,12 @@
 public class MusicFactory {
+
+    public MusicPlayer getMusicPlayer(MusicType musicType) {
+        MusicPlayer musicPlayer;
+        switch (musicType) {
+            case MP3 -> musicPlayer = new Mp3Player();
+            case OGG -> musicPlayer = new OggPlayer();
+            default -> throw new IllegalArgumentException("this type is not implemented");
+        }
+        return musicPlayer;
+    }
 }
