35a684d1ee2ffff5693c4669c0ecdec275d38019
farzadafi <farzadafi40@gmail.com>
2023-02-14 06:26:17 +0330

    add main method

--- MusicPlayer/src/main/java/Client.java
+++ MusicPlayer/src/main/java/Client.java
@@ -1,3 +1,8 @@
 public class Client {
+    public static void main(String[] args) {
+        MusicFactory musicFactory = new MusicFactory();
 
+        MusicPlayer musicPlayer = musicFactory.getMusicPlayer(MusicType.OGG);
+        musicPlayer.play();
+    }
 }
