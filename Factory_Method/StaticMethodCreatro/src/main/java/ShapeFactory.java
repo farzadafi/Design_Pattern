cae4a71ba6776915b94e45e790a32b4295fc7952
farzadafi <farzadafi40@gmail.com>
2023-02-14 06:52:47 +0330

    add getShape method

--- Factory_Method/StaticMethodCreatro/src/main/java/ShapeFactory.java
+++ Factory_Method/StaticMethodCreatro/src/main/java/ShapeFactory.java
@@ -1,4 +1,16 @@
 public class ShapeFactory {
 
+    public static Shape getShape(String shapeType) {
+        Shape shape = null;
+        if (shapeType == null)
+            return null;
+        else if (shapeType.equalsIgnoreCase("CIRCLE"))
+            shape = new Circle();
+        else if (shapeType.equalsIgnoreCase("RECTANGLE"))
+            shape = new Rectangle();
+        else if (shapeType.equalsIgnoreCase("SQUARE"))
+            shape = new Square();
 
+        return shape;
+    }
 }
