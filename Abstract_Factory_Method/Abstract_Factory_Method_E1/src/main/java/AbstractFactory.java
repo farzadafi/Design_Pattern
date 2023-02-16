d8f2b4aa4e70f5d468b3c814d0fc9dfd431b0c8b
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:47:01 +0330

    create ColorFactory class and extend from AbstractFactory

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
@@ -25,3 +25,16 @@ class ShapeFactory extends AbstractFactory {
     }
 
 }
+
+class ColorFactory extends AbstractFactory {
+
+    @Override
+    Color getColor(String color) {
+        return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+        return null;
+    }
+}
