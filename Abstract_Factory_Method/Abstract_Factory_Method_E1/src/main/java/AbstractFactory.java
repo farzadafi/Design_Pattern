b568da8f269776fb76a3916c638da985a0f172e6
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:46:04 +0330

    add ShapeFactory class and extend from AbstractFactory

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
@@ -2,3 +2,17 @@ public abstract class AbstractFactory {
     abstract Color getColor(String color);
     abstract Shape getShape(String shape);
 }
+
+class ShapeFactory extends AbstractFactory {
+
+    @Override
+    Shape getShape(String shape) {
+        return null;
+    }
+
+    @Override
+    Color getColor(String color) {
+        return null;
+    }
+
+}
