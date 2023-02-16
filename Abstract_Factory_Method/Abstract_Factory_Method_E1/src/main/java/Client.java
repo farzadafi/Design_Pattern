026b688003775eec9048ffb0737bb3e3d423a812
farzadafi <farzadafi40@gmail.com>
2023-02-16 13:21:59 +0330

    fix getShape input parameter

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
@@ -2,13 +2,13 @@ public class Client {
     public static void main(String[] args) {
         AbstractFactory shapeFactory = FactoryProducer.getFactory(FactoryName.SHAPE);
 
-        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        Shape shape1 = shapeFactory.getShape(ShapeType.CIRCLE);
         shape1.draw();
 
-        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        Shape shape2 = shapeFactory.getShape(ShapeType.RECTANGLE);
         shape2.draw();
 
-        Shape shape3 = shapeFactory.getShape("SQUARE");
+        Shape shape3 = shapeFactory.getShape(ShapeType.SQUARE);
         shape3.draw();
 
         AbstractFactory colorFactory = FactoryProducer.getFactory(FactoryName.COLOR);
