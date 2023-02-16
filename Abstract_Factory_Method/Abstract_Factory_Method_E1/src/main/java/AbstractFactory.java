8a81f3c2bd1b174e727e69f13bc8d426e6271d47
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:46:26 +0330

    implement body of getShape method

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
@@ -7,6 +7,15 @@ class ShapeFactory extends AbstractFactory {
 
     @Override
     Shape getShape(String shape) {
+        if(shape == null )
+            return null;
+        if(shape.equalsIgnoreCase("CIRCLE"))
+            return new Circle();
+        else if(shape.equalsIgnoreCase("RECTANGLE"))
+            return new Rectangle();
+        else if(shape.equalsIgnoreCase("SQUARE"))
+            return new Square();
+
         return null;
     }
 
