1d61ba9b679ae0c62c529093ba06c3035e3013c9
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:55:07 +0330

    add some test for colorFactory

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
@@ -11,5 +11,17 @@ public class Client {
 
         Shape shape3 = shapeFactory.getShape("SQUARE");
         shape3.draw();
+
+        AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
+
+        assert colorFactory != null;
+        Color color1 = colorFactory.getColor("GREEN");
+        color1.fill();
+
+        Color color2 = colorFactory.getColor("BLUE");
+        color2.fill();
+
+        Color color3 = colorFactory.getColor("RED");
+        color3.fill();
     }
 }
