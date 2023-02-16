5822ba698259694baff19c68f74b509da529adf6
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:53:13 +0330

    add main method and add some test for shapeFactory

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/Client.java
@@ -1,2 +1,15 @@
 public class Client {
+    public static void main(String[] args) {
+        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+
+        assert shapeFactory != null;
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+        shape3.draw();
+    }
 }
