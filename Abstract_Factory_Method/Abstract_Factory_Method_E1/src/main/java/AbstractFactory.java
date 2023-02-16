7ef7ba870871a91287f3e711e27153ae6b744d17
farzadafi <farzadafi40@gmail.com>
2023-02-16 12:48:26 +0330

    implement body of getColor method

--- Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
+++ Abstract_Factory_Method/Abstract_Factory_Method_E1/src/main/java/AbstractFactory.java
@@ -30,6 +30,16 @@ class ColorFactory extends AbstractFactory {
 
     @Override
     Color getColor(String color) {
+        if(color == null)
+            return null;
+
+        if(color.equalsIgnoreCase("RED"))
+            return new Red();
+        else if(color.equalsIgnoreCase("GREEN"))
+            return new Green();
+        else if(color.equalsIgnoreCase("BLUE"))
+            return new Blue();
+
         return null;
     }
 
