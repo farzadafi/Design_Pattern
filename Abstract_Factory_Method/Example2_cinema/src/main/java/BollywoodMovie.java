9f0103c2e463d045fa24a160bb0c1cb95032fd27
farzadafi <farzadafi40@gmail.com>
2023-02-17 06:15:37 +0330

    add BollywoodComedy and implements BollywoodMovie

--- Abstract_Factory_Method/Example2_cinema/src/main/java/BollywoodMovie.java
+++ Abstract_Factory_Method/Example2_cinema/src/main/java/BollywoodMovie.java
@@ -1,3 +1,11 @@
 public interface BollywoodMovie {
     void create();
 }
+
+class BollywoodComedy implements BollywoodMovie {
+
+    @Override
+    public void create() {
+        System.out.println("This action movie created in Bollywood!");
+    }
+}
