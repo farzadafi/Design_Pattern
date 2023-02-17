f52ac2deecef8dc20822bd4be33984db78bd1a81
farzadafi <farzadafi40@gmail.com>
2023-02-17 06:12:52 +0330

    add HollyWoodComedy and implements HollywoodMovie

--- Abstract_Factory_Method/Example2_cinema/src/main/java/HollywoodMovie.java
+++ Abstract_Factory_Method/Example2_cinema/src/main/java/HollywoodMovie.java
@@ -1,3 +1,11 @@
 public interface HollywoodMovie {
     void create();
 }
+
+class HollywoodComedy implements HollywoodMovie {
+
+    @Override
+    public void create() {
+        System.out.println("This comedy movie created on hollywood");
+    }
+}
