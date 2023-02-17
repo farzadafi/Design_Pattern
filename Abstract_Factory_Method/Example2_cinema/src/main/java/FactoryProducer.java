14721aae89e935e45d84a99eb924b32a9de06217
farzadafi <farzadafi40@gmail.com>
2023-02-17 06:30:04 +0330

    add getFactory method

--- Abstract_Factory_Method/Example2_cinema/src/main/java/FactoryProducer.java
+++ Abstract_Factory_Method/Example2_cinema/src/main/java/FactoryProducer.java
@@ -1,5 +1,13 @@
 public class FactoryProducer {
 
+    public static AbstractFactory getFactory(FactoryType factoryType) {
+        AbstractFactory abstractFactory = null;
+        switch (factoryType) {
+            case BOLLYWOOD -> abstractFactory = new BollywoodFactory();
+            case HOLLYWOOD -> abstractFactory = new HollywoodFactory();
+        }
+        return abstractFactory;
+    }
 }
 
 enum FactoryType {
