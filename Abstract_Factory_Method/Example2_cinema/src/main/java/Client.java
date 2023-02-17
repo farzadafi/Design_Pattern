6f09e9ea6ad1ff81cb2bdd93dc985417a676dea5
farzadafi <farzadafi40@gmail.com>
2023-02-17 06:37:25 +0330

    add main method for test

--- Abstract_Factory_Method/Example2_cinema/src/main/java/Client.java
+++ Abstract_Factory_Method/Example2_cinema/src/main/java/Client.java
@@ -1,2 +1,21 @@
 public class Client {
+    public static void main(String[] args) {
+        AbstractFactory hollywoodFactory = FactoryProducer.getFactory(FactoryType.HOLLYWOOD);
+
+        HollywoodMovie hollywoodComedy = hollywoodFactory.getHollywoodFactory(Genera.COMEDY);
+        hollywoodComedy.create();
+
+        HollywoodMovie hollywoodAction = hollywoodFactory.getHollywoodFactory(Genera.ACTION);
+        hollywoodAction.create();
+
+        System.out.println();
+        //-->Bollywood
+        AbstractFactory bollywoodFactory = FactoryProducer.getFactory(FactoryType.BOLLYWOOD);
+
+        BollywoodMovie bollywoodComedy = bollywoodFactory.getBollywoodFactory(Genera.COMEDY);
+        bollywoodComedy.create();
+
+        BollywoodMovie bollywoodAction = bollywoodFactory.getBollywoodFactory(Genera.ACTION);
+        bollywoodAction.create();
+    }
 }
