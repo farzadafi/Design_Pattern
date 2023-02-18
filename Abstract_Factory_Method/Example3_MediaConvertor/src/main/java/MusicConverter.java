bd2e102c8ddc3b0efb7db95c384c8f08816e7dbd
farzadafi <farzadafi40@gmail.com>
2023-02-18 05:59:12 +0330

    add Mp3ToAAC class and implements from MusicConverter interface

--- Abstract_Factory_Method/Example3_MediaConvertor/src/main/java/MusicConverter.java
+++ Abstract_Factory_Method/Example3_MediaConvertor/src/main/java/MusicConverter.java
@@ -1,3 +1,11 @@
 public interface MusicConverter {
     void convert();
 }
+
+class Mp3ToAAC implements MusicConverter {
+
+    @Override
+    public void convert() {
+        System.out.println("Mp3 to AAC convert!");
+    }
+}
