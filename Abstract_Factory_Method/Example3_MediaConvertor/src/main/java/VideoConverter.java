c9d285e7abeb83c4cfa3e67a32e313f6916efe8f
farzadafi <farzadafi40@gmail.com>
2023-02-18 06:01:38 +0330

    add Mp4ToWebm class and implements from VideoConverter

--- Abstract_Factory_Method/Example3_MediaConvertor/src/main/java/VideoConverter.java
+++ Abstract_Factory_Method/Example3_MediaConvertor/src/main/java/VideoConverter.java
@@ -1,3 +1,11 @@
 public interface VideoConverter {
     void convert();
 }
+
+class Mp4ToWebm implements VideoConverter {
+
+    @Override
+    public void convert() {
+        System.out.println("Mp4 To webm convert!");
+    }
+}
