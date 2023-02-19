61e69959d94349d809a457bd19a0d308da75fd94
farzadafi <farzadafi40@gmail.com>
2023-02-19 07:45:40 +0330

    add main method for test singleton

--- Singleton/Lazy_Initialization/src/main/java/Client.java
+++ Singleton/Lazy_Initialization/src/main/java/Client.java
@@ -1,2 +1,13 @@
 public class Client {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        singleton.setData(5);
+        System.out.printf("First reference is %s%n", singleton);
+        System.out.printf("Singleton data value is %s%n", singleton.getData());
+
+        singleton = Singleton.getInstance();
+        System.out.printf("%nSecond reference is %s%n", singleton);
+        System.out.printf("Singleton data value is %s%n", singleton.getData());
+
+    }
 }
