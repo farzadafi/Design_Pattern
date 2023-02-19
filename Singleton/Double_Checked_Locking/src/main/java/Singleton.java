b4f9ca5d650791595400f7dc794683dbbe6ee9af
farzadafi <farzadafi40@gmail.com>
2023-02-19 09:28:47 +0330

    write whole needed property for write and test Singleton class

--- Singleton/Double_Checked_Locking/src/main/java/Singleton.java
+++ Singleton/Double_Checked_Locking/src/main/java/Singleton.java
@@ -1,2 +1,24 @@
 public class Singleton {
+
+    private static Singleton Instance = null;
+
+    private int data = 0;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (Instance == null)
+            Instance = new Singleton();
+        return Instance;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
 }
