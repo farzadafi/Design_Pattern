14b617950bca3c64657bcf09c9f3bd142a667756
farzadafi <farzadafi40@gmail.com>
2023-02-19 07:40:44 +0330

    add getInstance method

--- Singleton/Lazy_Initialization/src/main/java/Singleton.java
+++ Singleton/Lazy_Initialization/src/main/java/Singleton.java
@@ -1,2 +1,16 @@
 public class Singleton {
+
+    private static Singleton Instance = null;
+
+    private int data = 0;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (Instance == null)
+            Instance = new Singleton();
+        return Instance;
+    }
 }
