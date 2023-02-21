0d92b03b6517cd9a7ba363ecd692f597732c3193
farzadafi <farzadafi40@gmail.com>
2023-02-21 06:20:47 +0330

    add a new SingletonHelper class and another Singleton methods

--- Singleton/Bill_Pugh/src/main/java/Singleton.java
+++ Singleton/Bill_Pugh/src/main/java/Singleton.java
@@ -1,2 +1,25 @@
 public class Singleton {
+
+    private int data = 0;
+
+    private Singleton() {
+
+    }
+
+    private static final class SingletonHelper {
+
+        private static final Singleton singleton = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHelper.singleton;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
 }
