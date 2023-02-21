62f0c407a611de96a19d8de47336eaabfddf3f73
farzadafi <farzadafi40@gmail.com>
2023-02-21 06:12:15 +0330

    add whole of singleton code class

--- Singleton/Eager_Evaluation/src/main/java/Singleton.java
+++ Singleton/Eager_Evaluation/src/main/java/Singleton.java
@@ -1,2 +1,22 @@
 public class Singleton {
+
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    private int data = 0;
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
 }
