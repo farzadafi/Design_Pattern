9266cdfff64c3859f3c602470a045747de45155c
farzadafi <farzadafi40@gmail.com>
2023-02-22 08:22:51 +0330

    add whole code need for singleton Captain

--- Singleton/Bill_Paug_Cricket/src/main/java/Captain.java
+++ Singleton/Bill_Paug_Cricket/src/main/java/Captain.java
@@ -1,2 +1,16 @@
 public class Captain {
+
+    private Captain () {
+
+    }
+
+    private static final class CaptainSingleton {
+
+        private static final Captain captain = new Captain();
+
+    }
+
+    public static Captain getInstance() {
+        return CaptainSingleton.captain;
+    }
 }
