50a0a5af6c8c78f1b8eb67bbf1c185861ff916f3
farzadafi <farzadafi40@gmail.com>
2023-02-22 08:23:12 +0330

    add main method for test Captain singleton class

--- Singleton/Bill_Paug_Cricket/src/main/java/Client.java
+++ Singleton/Bill_Paug_Cricket/src/main/java/Client.java
@@ -1,2 +1,8 @@
 public class Client {
+    public static void main(String[] args) {
+        Captain captain = Captain.getInstance();
+        System.out.println(captain);
+        Captain captain1 = Captain.getInstance();
+        System.out.println(captain1);
+    }
 }
