318c9702b73927ecd809ab65e7d0e6d657cb789f
farzadafi <farzadafi40@gmail.com>
2023-02-23 13:40:38 +0330

    add Car class and implements BuilderInterface

--- Builder/Vehicle_Example/src/main/java/BuilderInterface.java
+++ Builder/Vehicle_Example/src/main/java/BuilderInterface.java
@@ -4,3 +4,28 @@ public interface BuilderInterface {
     void addHeadlights();
     Product getVehicle();
 }
+
+class Car implements BuilderInterface {
+
+    private Product product = new Product();
+
+    @Override
+    public void buildBody() {
+        product.add("This is a body of a car");
+    }
+
+    @Override
+    public void insertWheels() {
+        product.add("4 wheels are added");
+    }
+
+    @Override
+    public void addHeadlights() {
+        product.add("2 headlights are added")
+    }
+
+    @Override
+    public Product getVehicle() {
+        return product;
+    }
+}
