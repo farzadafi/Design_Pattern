671f15209cfc37b015c4f5c3797fa820e2895862
farzadafi <farzadafi40@gmail.com>
2023-02-23 13:44:50 +0330

    add linkedList and show method

--- Builder/Vehicle_Example/src/main/java/Product.java
+++ Builder/Vehicle_Example/src/main/java/Product.java
@@ -1,2 +1,23 @@
+import java.util.LinkedList;
+
 public class Product {
+
+    private LinkedList<String> parts;
+
+    public Product() {
+        parts = new LinkedList<>();
+    }
+
+    public void add(String part) {
+        parts.addLast(part);
+    }
+
+    public void show() {
+        System.out.println("\n product completed as below");
+
+        for (String s:parts
+             ) {
+            System.out.println(s);
+        }
+    }
 }
