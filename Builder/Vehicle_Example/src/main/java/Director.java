2179316ad546b462f855cf427f7cecd4167806cb
farzadafi <farzadafi40@gmail.com>
2023-02-23 13:47:21 +0330

    add construct method

--- Builder/Vehicle_Example/src/main/java/Director.java
+++ Builder/Vehicle_Example/src/main/java/Director.java
@@ -1,2 +1,11 @@
 public class Director {
+
+    BuilderInterface builderInterface;
+
+    public void construct (BuilderInterface builderInterface) {
+        this.builderInterface = builderInterface;
+        this.builderInterface.buildBody();
+        this.builderInterface.insertWheels();
+        this.builderInterface.addHeadlights();
+    }
 }
