a54ea8572fdb9d97ca4a2fa6f7f582915ec3f9c9
farzadafi <farzadafi40@gmail.com>
2023-02-23 13:42:12 +0330

    add MotorCycle class and implements BuilderInterface

--- Builder/Vehicle_Example/src/main/java/BuilderInterface.java
+++ Builder/Vehicle_Example/src/main/java/BuilderInterface.java
@@ -29,3 +29,28 @@ class Car implements BuilderInterface {
         return product;
     }
 }
+
+class MotorCycle implements BuilderInterface {
+
+    private Product product = new Procuct();
+
+    @Override
+    public void buildBody() {
+        product.add("This is a body of a MotorCycle");
+    }
+
+    @Override
+    public void insertWheels() {
+        product.add("2 wheels are added");
+    }
+
+    @Override
+    public void addHeadlights() {
+        product.add("1 headlights are added")
+    }
+
+    @Override
+    public Product getVehicle() {
+        return product;
+    }
+}
