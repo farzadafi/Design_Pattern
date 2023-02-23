373b7306d6154b7997ae3b1c5dde3d05afa87b8b
farzadafi <farzadafi40@gmail.com>
2023-02-23 13:51:16 +0330

    add main method for test

--- Builder/Vehicle_Example/src/main/java/Client.java
+++ Builder/Vehicle_Example/src/main/java/Client.java
@@ -1,2 +1,18 @@
 public class Client {
+    public static void main(String[] args) {
+        Director director = new Director();
+
+        BuilderInterface carBuilder = new Car();
+        BuilderInterface motorBuilder = new MotorCycle();
+
+        //making a car
+        director.construct(carBuilder);
+        Product product1 = carBuilder.getVehicle();
+        product1.show();
+
+        //making a motor
+        director.construct(motorBuilder);
+        Product product2 = motorBuilder.getVehicle();
+        product2.show();
+    }
 }
