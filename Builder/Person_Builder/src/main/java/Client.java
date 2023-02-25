ebf16aab95aaf1e09062acfbeb247ede7ada6804
farzadafi <farzadafi40@gmail.com>
2023-02-25 13:46:42 +0330

    add main method for test

--- Builder/Person_Builder/src/main/java/Client.java
+++ Builder/Person_Builder/src/main/java/Client.java
@@ -1,2 +1,11 @@
 public class Client {
+    public static void main(String[] args) {
+        Person person = Person.builder()
+                .firstname("farzad")
+                .lastname("afshar")
+                .username("farzadafi")
+                .password("naser")
+                .build();
+        System.out.println(person);
+    }
 }
