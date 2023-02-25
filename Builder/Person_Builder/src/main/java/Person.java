454c7444a9e0ca295df644d7a9551e5b3d53f3f8
farzadafi <farzadafi40@gmail.com>
2023-02-25 13:46:32 +0330

    add Builder class and add method use for create a builder

--- Builder/Person_Builder/src/main/java/Person.java
+++ Builder/Person_Builder/src/main/java/Person.java
@@ -4,4 +4,44 @@ public class Person {
     private String lastname;
     private String username;
     private String password;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String firstname;
+        private String lastname;
+        private String username;
+        private String password;
+
+        public Builder firstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public Builder lastname(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Person build() {
+            Person person = new Person();
+            person.firstname = firstname;
+            person.lastname = lastname;
+            person.username = username;
+            person.password = password;
+            return person;
+        }
+    }
 }
