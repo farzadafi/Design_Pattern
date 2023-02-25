0bd3d41ca43236a45632cbd06c9371bf51ee75f9
farzadafi <farzadafi40@gmail.com>
2023-02-25 05:52:42 +0330

    add new two field and fix constructor and add new Getter and Setter

--- Builder/Bank_Account/src/main/java/BankAccount.java
+++ Builder/Bank_Account/src/main/java/BankAccount.java
@@ -3,11 +3,31 @@ public class BankAccount {
     private long accountNumber;
     private String owner;
     private double balance;
+    private String branch;
+    private double interestRate;
 
-    public BankAccount(long accountNumber, String owner, double balance) {
+    public BankAccount(long accountNumber, String owner, double balance, String branch, double interestRate) {
         this.accountNumber = accountNumber;
         this.owner = owner;
         this.balance = balance;
+        this.branch = branch;
+        this.interestRate = interestRate;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
     }
 
     public long getAccountNumber() {
