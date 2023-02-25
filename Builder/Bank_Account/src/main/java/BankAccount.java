e98a5a545497e8edab7a0ad6c6659c31bffe6972
farzadafi <farzadafi40@gmail.com>
2023-02-25 05:51:20 +0330

    add Getter and Setter

--- Builder/Bank_Account/src/main/java/BankAccount.java
+++ Builder/Bank_Account/src/main/java/BankAccount.java
@@ -9,4 +9,28 @@ public class BankAccount {
         this.owner = owner;
         this.balance = balance;
     }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
