e75a04bae7e54a4dc2d2dc212bdab5459a9c74e3
farzadafi <farzadafi40@gmail.com>
2023-02-25 05:57:35 +0330

    add new static class for Builder

--- Builder/Bank_Account/src/main/java/BankAccount.java
+++ Builder/Bank_Account/src/main/java/BankAccount.java
@@ -53,4 +53,51 @@ public class BankAccount {
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+
+    public static class Builder {
+
+        private final long accountNumber;
+        private String owner;
+        private String branch;
+        private double balance;
+        private double interestRate;
+
+        public Builder(long accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public Builder withOwner(String owner){
+            this.owner = owner;
+            return this;
+        }
+
+        public Builder atBranch(String branch){
+            this.branch = branch;
+            return this;
+        }
+
+        public Builder openingBalance(double balance){
+            this.balance = balance;
+            return this;
+        }
+
+        public Builder atRate(double interestRate){
+            this.interestRate = interestRate;
+            return this;
+        }
+
+        public BankAccount build(){
+            BankAccount account = new BankAccount();
+            account.accountNumber = this.accountNumber;
+            account.owner = this.owner;
+            account.branch = this.branch;
+            account.balance = this.balance;
+            account.interestRate = this.interestRate;
+            return account;
+        }
+    }
+
+    private BankAccount() {
+    }
 }
