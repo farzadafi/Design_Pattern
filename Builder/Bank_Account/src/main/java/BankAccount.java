11ceb9e9c3a9b35c215e16ea0c0a802877e60828
farzadafi <farzadafi40@gmail.com>
2023-02-25 05:51:02 +0330

    added all need filed

--- Builder/Bank_Account/src/main/java/BankAccount.java
+++ Builder/Bank_Account/src/main/java/BankAccount.java
@@ -1,2 +1,12 @@
 public class BankAccount {
+
+    private long accountNumber;
+    private String owner;
+    private double balance;
+
+    public BankAccount(long accountNumber, String owner, double balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
 }
