de8d3e9578d90775cb06e487e11292ae32d14459
farzadafi <farzadafi40@gmail.com>
2023-02-25 05:59:53 +0330

    add main mehthod for test

--- Builder/Bank_Account/src/main/java/Client.java
+++ Builder/Bank_Account/src/main/java/Client.java
@@ -1,2 +1,14 @@
 public class Client {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount.Builder(1234L)
+                .withOwner("Farzad")
+                .atBranch("Afshar")
+                .openingBalance(100)
+                .atRate(2.5)
+                .build();
+        BankAccount account1 = new BankAccount("farzad", 12, "zarand", 12);
+
+        System.out.println(account);
+        System.out.println(account1);
+    }
 }
