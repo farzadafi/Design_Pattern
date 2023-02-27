dc739c84bf5cd61c9d70e59c123b8ddad33c62f7
farzadafi <farzadafi40@gmail.com>
2023-02-27 17:18:16 +0330

    add getShape method

--- Prototype/Example1_Shape/src/main/java/ShapeCache.java
+++ Prototype/Example1_Shape/src/main/java/ShapeCache.java
@@ -1,2 +1,10 @@
+import java.util.Hashtable;
+
 public class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeHashtable = new Hashtable<>();
+
+    public static Shape getShape(String shapeId) {
+        return shapeHashtable.get((shapeId));
+    }
 }
