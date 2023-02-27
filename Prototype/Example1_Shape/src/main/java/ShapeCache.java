2519b83cfbac907354d14478be35aa84fe106321
farzadafi <farzadafi40@gmail.com>
2023-02-27 17:20:04 +0330

    add loadCached method

--- Prototype/Example1_Shape/src/main/java/ShapeCache.java
+++ Prototype/Example1_Shape/src/main/java/ShapeCache.java
@@ -7,4 +7,18 @@ public class ShapeCache {
     public static Shape getShape(String shapeId) {
         return shapeHashtable.get((shapeId));
     }
+
+    public static void loadCache() {
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeHashtable.put(circle.getId(), circle);
+
+        Sqaure sqaure = new Sqaure();
+        sqaure.setId("2");
+        shapeHashtable.put(sqaure.getId(), sqaure);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeHashtable.put(rectangle.getId(), rectangle);
+    }
 }
