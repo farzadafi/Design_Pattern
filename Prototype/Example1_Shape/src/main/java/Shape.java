c6b57cdbc74820e6f404edf9a04e86d1f70d65b1
farzadafi <farzadafi40@gmail.com>
2023-02-27 17:13:14 +0330

    add clone method

--- Prototype/Example1_Shape/src/main/java/Shape.java
+++ Prototype/Example1_Shape/src/main/java/Shape.java
@@ -1,4 +1,4 @@
-public abstract class Shape {
+public abstract class Shape implements Cloneable {
     private String id;
     protected String type;
 
@@ -18,5 +18,17 @@ public abstract class Shape {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+        }catch (CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        return clone;
     }
 }
