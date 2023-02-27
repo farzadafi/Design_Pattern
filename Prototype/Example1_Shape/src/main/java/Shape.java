625aed0b6f84297d9e0524e28291f70de6fd9cbf
farzadafi <farzadafi40@gmail.com>
2023-02-27 17:11:52 +0330

    add needed field and add getter and setter

--- Prototype/Example1_Shape/src/main/java/Shape.java
+++ Prototype/Example1_Shape/src/main/java/Shape.java
@@ -1,2 +1,22 @@
-public class Shape {
+public abstract class Shape {
+    private String id;
+    protected String type;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
