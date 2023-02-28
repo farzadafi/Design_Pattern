ac7a5120b92d9808af68bef170a4002e6f8b6090
farzadafi <farzadafi40@gmail.com>
2023-02-28 15:38:36 +0330

    add getter and setter and constructor

--- Prototype/Example2_Employee_CopyConstructor/src/main/java/Employee.java
+++ Prototype/Example2_Employee_CopyConstructor/src/main/java/Employee.java
@@ -1,2 +1,28 @@
 public class Employee {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Employee() {
+    }
 }
