7b07054a3918365514fff304f9c7163ef719751f
farzadafi <farzadafi40@gmail.com>
2023-02-28 14:03:17 +0330

    add main method for test

--- Prototype/Example1_Shape/src/main/java/Client.java
+++ Prototype/Example1_Shape/src/main/java/Client.java
@@ -1,2 +1,14 @@
 public class Client {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape shape1 = ShapeCache.getShape("1");
+        System.out.println("Shape " + shape1.getType());
+
+        Shape shape2 = ShapeCache.getShape("2");
+        System.out.println("Shape " + shape2.getType());
+
+        Shape shape3 = ShapeCache.getShape("3");
+        System.out.println("Shape " + shape3.getType());
+    }
 }
