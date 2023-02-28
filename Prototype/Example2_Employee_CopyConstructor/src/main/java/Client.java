12262cf834123b1f3fc97d78fd9449355040fd9f
farzadafi <farzadafi40@gmail.com>
2023-02-28 15:45:31 +0330

    add main method for test

--- Prototype/Example2_Employee_CopyConstructor/src/main/java/Client.java
+++ Prototype/Example2_Employee_CopyConstructor/src/main/java/Client.java
@@ -1,2 +1,11 @@
+import java.util.Date;
+
 public class Client {
+    public static void main(String[] args) {
+        Employee orginalEmployee = new Employee(1, "farzad", new Date());
+        System.out.println("original Employee is: " + orginalEmployee);
+
+        Employee copyEmployee = new Employee(orginalEmployee);
+        System.out.println("\ncopy Employee is: " + copyEmployee);
+    }
 }
