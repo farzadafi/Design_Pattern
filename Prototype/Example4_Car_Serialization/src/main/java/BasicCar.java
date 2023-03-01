28ff44ca21cdd31fe1ae73f99c73a60bad966446
farzadafi <farzadafi40@gmail.com>
2023-03-01 14:51:34 +0330

    add constructor and toString method

--- Prototype/Example4_Car_Serialization/src/main/java/BasicCar.java
+++ Prototype/Example4_Car_Serialization/src/main/java/BasicCar.java
@@ -1,2 +1,20 @@
+import java.io.*;
+
 public class BasicCar {
+    private String modelName;
+    private Long price;
+
+    public BasicCar(String modelName, Long price) {
+        this.modelName = modelName;
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BasicCar{" +
+                "modelName='" + modelName + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
