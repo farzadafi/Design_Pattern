990c4c9d4bd78fbff76eb8f49d471810b8465ba4
farzadafi <farzadafi40@gmail.com>
2023-03-01 14:48:43 +0330

    add main method for test

--- Prototype/Example3_Car_Clone/src/main/java/Client.java
+++ Prototype/Example3_Car_Clone/src/main/java/Client.java
@@ -1,2 +1,11 @@
 public class Client {
+    public static void main(String[] args) {
+        BasicCarCache carCache = new BasicCarCache();
+
+        BasicCar basicCar = carCache.getBasicCar("nano");
+        System.out.println(basicCar);
+
+        BasicCar basicCar1 = carCache.getBasicCar("ford");
+        System.out.println(basicCar1);
+    }
 }
