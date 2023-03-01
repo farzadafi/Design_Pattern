3dad7c7781be06fc6717806956fbc6ba8ce7dedc
farzadafi <farzadafi40@gmail.com>
2023-03-01 15:14:01 +0330

    add main method for test

--- Prototype/Example4_Car_Serialization/src/main/java/Client.java
+++ Prototype/Example4_Car_Serialization/src/main/java/Client.java
@@ -1,2 +1,11 @@
+import java.io.IOException;
+
 public class Client {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        BasicCar basicCar = new BasicCar("ford", 4000L);
+        System.out.println(basicCar.hashCode());
+
+        BasicCar basicCar1 = basicCar.copy(basicCar);
+        System.out.println(basicCar1.hashCode());
+    }
 }
