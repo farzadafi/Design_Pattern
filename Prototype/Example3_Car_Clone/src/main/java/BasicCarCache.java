ee2ef820a9987132c6f66ce750cd4d532eab6d56
farzadafi <farzadafi40@gmail.com>
2023-03-01 14:23:39 +0330

    add initialize block

--- Prototype/Example3_Car_Clone/src/main/java/BasicCarCache.java
+++ Prototype/Example3_Car_Clone/src/main/java/BasicCarCache.java
@@ -2,13 +2,16 @@ import java.util.Hashtable;
 
 public class BasicCarCache {
 
-    private Hashtable<String, BasicCar> hashtable = new Hashtable<>();
+    private final Hashtable<String, BasicCar> hashtable = new Hashtable<>();
+
+    {
+        hashtable.put("ford", new Ford("ford", 4000L));
+        hashtable.put("nano", new Nano("nano", 5000L));
+    }
 
     public BasicCar getBasicCar(String type) {
         if(hashtable.containsKey(type))
             return hashtable.get(type).clone();
         return null;
     }
-
-
 }
