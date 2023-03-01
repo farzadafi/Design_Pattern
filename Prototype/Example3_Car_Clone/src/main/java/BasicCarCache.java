11342cad8779ffd2ffb01ae00aeff8c20b893e2b
farzadafi <farzadafi40@gmail.com>
2023-03-01 14:21:54 +0330

    add getBasicCar method

--- Prototype/Example3_Car_Clone/src/main/java/BasicCarCache.java
+++ Prototype/Example3_Car_Clone/src/main/java/BasicCarCache.java
@@ -1,2 +1,14 @@
+import java.util.Hashtable;
+
 public class BasicCarCache {
+
+    private Hashtable<String, BasicCar> hashtable = new Hashtable<>();
+
+    public BasicCar getBasicCar(String type) {
+        if(hashtable.containsKey(type))
+            return hashtable.get(type).clone();
+        return null;
+    }
+
+
 }
