142e54907ff5b6ef6eeb233b2c5167fd202fa96a
farzadafi <farzadafi40@gmail.com>
2023-03-04 13:50:50 +0330

    implements from Duck and override two method

--- Adapter/A_TurkeyDuck_Example/src/main/java/AdapterTurkey.java
+++ Adapter/A_TurkeyDuck_Example/src/main/java/AdapterTurkey.java
@@ -1,2 +1,20 @@
-public class AdapterTurkey {
+public class AdapterTurkey implements Duck{
+
+    Turkey turkey;
+
+    public AdapterTurkey(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+
+    @Override
+    public void quack() {
+        turkey.gobble();
+    }
+
+    @Override
+    public void fly() {
+        for (int i = 0;i<3;i++)
+            turkey.fly();
+    }
 }
