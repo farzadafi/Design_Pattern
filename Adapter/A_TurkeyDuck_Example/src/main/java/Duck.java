1831d8329c7efae72a6f129fe21ff8fec3452428
farzadafi <farzadafi40@gmail.com>
2023-03-04 13:43:31 +0330

    add MallardDuck and implements from Duck interface

--- Adapter/A_TurkeyDuck_Example/src/main/java/Duck.java
+++ Adapter/A_TurkeyDuck_Example/src/main/java/Duck.java
@@ -2,3 +2,16 @@ public interface Duck {
     void quack();
     void fly();
 }
+
+class MallardDuck implements Duck {
+
+    @Override
+    public void quack() {
+        System.out.println("Quack!");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I am flying");
+    }
+}
