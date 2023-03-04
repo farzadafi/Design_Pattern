3071333a63c70498d0520d5284240d7683243b56
farzadafi <farzadafi40@gmail.com>
2023-03-04 13:45:26 +0330

    add WildTurkey class and implements from Turkey interface

--- Adapter/A_TurkeyDuck_Example/src/main/java/Turkey.java
+++ Adapter/A_TurkeyDuck_Example/src/main/java/Turkey.java
@@ -2,3 +2,16 @@ public interface Turkey {
     void gobble();
     void fly();
 }
+
+class WildTurkey implements Turkey {
+
+    @Override
+    public void gobble() {
+        System.out.println("Gobble gobble!");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I am flying a short distance");
+    }
+}
