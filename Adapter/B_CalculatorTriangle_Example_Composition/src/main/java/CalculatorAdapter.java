edf747e21f54c3a31694287a8cefd963e9561b97
farzadafi <farzadafi40@gmail.com>
2023-03-05 07:35:16 +0330

    add getArea method

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/CalculatorAdapter.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/CalculatorAdapter.java
@@ -1,2 +1,17 @@
-public class CalculatorAdapter {
+public class CalculatorAdapter implements Calculator{
+
+    private final Triangle triangle;
+
+    public CalculatorAdapter(Triangle triangle) {
+        this.triangle = triangle;
+    }
+
+    @Override
+    public double getArea(Rectangle rectangle) {
+        CalculatorImpel calculatorImpel = new CalculatorImpel();
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.length = triangle.base;
+        rectangle1.width = 0.5 * triangle.height;
+        return calculatorImpel.getArea(rectangle1);
+    }
 }
