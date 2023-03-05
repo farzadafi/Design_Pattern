e0708a9faa5e9dc0a47c8754ff1534113e84ecd5
farzadafi <farzadafi40@gmail.com>
2023-03-05 07:34:25 +0330

    add main method for test

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Client.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Client.java
@@ -1,2 +1,7 @@
 public class Client {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(20, 10);
+        Calculator calculatorAdapter = new CalculatorAdapter(triangle);
+        System.out.println(calculatorAdapter.getArea(null));
+    }
 }
