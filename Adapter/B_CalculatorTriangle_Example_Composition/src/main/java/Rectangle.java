7ec1b3013757b20f52f60ce028b8cc0f3331ae94
farzadafi <farzadafi40@gmail.com>
2023-03-05 07:35:42 +0330

    add new empty constructor

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
@@ -2,9 +2,7 @@ public class Rectangle {
     public double length;
     public double width;
 
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+    public Rectangle() {
     }
 }
 
