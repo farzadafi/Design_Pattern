ae8ecb0f3a62926181050115fbcb8b0f257bbec7
farzadafi <farzadafi40@gmail.com>
2023-03-05 08:09:57 +0330

    add IntegerValueImpel

--- Adapter/C_IntegerValue_ClassAdapter/src/main/java/IntegerValue.java
+++ Adapter/C_IntegerValue_ClassAdapter/src/main/java/IntegerValue.java
@@ -1,3 +1,11 @@
 public interface IntegerValue {
     int getInteger();
 }
+
+class IntegerValueImpel implements IntegerValue {
+
+    @Override
+    public int getInteger() {
+        return 5;
+    }
+}
