45fc560ed731045a288350ebfd31a0e13675c698
farzadafi <farzadafi40@gmail.com>
2023-03-05 06:55:38 +0330

    add main method for test

--- Adapter/A_TurkeyDuck_Example/src/main/java/Client.java
+++ Adapter/A_TurkeyDuck_Example/src/main/java/Client.java
@@ -1,2 +1,14 @@
 public class Client {
+    public static void main(String[] args) {
+        Duck duck = new MallardDuck();
+        duck.quack();
+
+        Turkey turkey = new WildTurkey();
+        turkey.gobble();
+
+        Duck adapterTurkey = new AdapterTurkey(turkey);
+        System.out.println("\nadapter:");
+        adapterTurkey.quack();
+        adapterTurkey.fly();
+    }
 }
