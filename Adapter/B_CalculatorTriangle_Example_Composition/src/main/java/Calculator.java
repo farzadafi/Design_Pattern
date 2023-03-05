8d11e6e29edc39f699576025886f6a9b08cb60bd
farzadafi <farzadafi40@gmail.com>
2023-03-05 07:06:00 +0330

    add CalculatorImpel class and implements from Calculator interface

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Calculator.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Calculator.java
@@ -1,3 +1,14 @@
 public interface Calculator {
     double getArea(Rectangle rectangle);
 }
+
+class CalculatorImpel implements Calculator {
+
+    private Rectangle rectangle;
+
+    @Override
+    public double getArea(Rectangle rectangle) {
+        this.rectangle = rectangle;
+        return rectangle.lengh * rectangle.width;
+    }
+}
