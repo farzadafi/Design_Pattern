a18ac61f93cb0bacc5247c1c2e2306d67e856bbf
farzadafi <farzadafi40@gmail.com>
2023-03-05 08:15:07 +0330

    add main method for test

--- Adapter/C_IntegerValue_ClassAdapter/src/main/java/Client.java
+++ Adapter/C_IntegerValue_ClassAdapter/src/main/java/Client.java
@@ -1,2 +1,9 @@
 public class Client {
+    public static void main(String[] args) {
+        ClassAdapter classAdapter = new ClassAdapter();
+        System.out.println("Class Adapter is returning " + classAdapter.getInteger());
+
+        ObjectAdapter objectAdapter = new ObjectAdapter(new IntegerValueImpel());
+        System.out.println("\nObject Adapter is returning " + objectAdapter.getInteger());
+    }
 }
