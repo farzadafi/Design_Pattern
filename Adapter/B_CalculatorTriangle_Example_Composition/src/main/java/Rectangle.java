d2a74430795ffc3df62c5fbd02f86133e076fe1a
farzadafi <farzadafi40@gmail.com>
2023-03-05 07:07:55 +0330

    add constructor to Rectangle class

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
@@ -1,6 +1,11 @@
 public class Rectangle {
-    public double lengh;
+    public double length;
     public double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
 }
 
 class Triangle {
