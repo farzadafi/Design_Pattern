8f57a32b88f5550407ca94e282961114abef0dfb
farzadafi <farzadafi40@gmail.com>
2023-03-05 06:59:52 +0330

    add Triangle class and add field

--- Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
+++ Adapter/B_CalculatorTriangle_Example_Composition/src/main/java/Rectangle.java
@@ -2,3 +2,13 @@ public class Rectangle {
     public double lengh;
     public double width;
 }
+
+class Triangle {
+    public double base;
+    public double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+}
