3f2cbb24f3026c0a665ff4e31f11d750885afa13
farzadafi <farzadafi40@gmail.com>
2023-03-06 13:02:55 +0330

    implements from MediaPlayer and override play method

--- Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayerAdapter.java
+++ Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayerAdapter.java
@@ -1,2 +1,14 @@
-public class AdvanceMediaPlayerAdapter {
+public class AdvanceMediaPlayerAdapter implements MediaPlayer{
+
+    private AdvanceMediaPlayer advanceMediaPlayer;
+
+    public AdvanceMediaPlayerAdapter(AdvanceMediaPlayer advanceMediaPlayer) {
+        this.advanceMediaPlayer = advanceMediaPlayer;
+    }
+
+    @Override
+    public void play(String fileName) {
+        advanceMediaPlayer.loadFileName(fileName);
+        advanceMediaPlayer.listen();
+    }
 }
