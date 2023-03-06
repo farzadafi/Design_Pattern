4e2ac6e4936c38f13edc66c787952b3474975051
farzadafi <farzadafi40@gmail.com>
2023-03-06 13:38:49 +0330

    add main method for test

--- Adapter/D_MediaPlayer/src/main/java/Client.java
+++ Adapter/D_MediaPlayer/src/main/java/Client.java
@@ -1,2 +1,14 @@
 public class Client {
+    public static void main(String[] args) {
+        MediaPlayer audioPlayer = new AudioPlayer();
+        audioPlayer.play("mp3", "farzad.mp3");
+
+        AdvanceMediaPlayer mp4Player = new Mp4Player();
+        MediaPlayer advanceMediaPlayerAdaptor1 = new AdvanceMediaPlayerAdapter(mp4Player);
+        advanceMediaPlayerAdaptor1.play("mp4", "farzad.mp4");
+
+        AdvanceMediaPlayer vlcPlayer = new VlcPlayer();
+        MediaPlayer advanceMediaPlayerAdaptor2 = new AdvanceMediaPlayerAdapter(vlcPlayer);
+        advanceMediaPlayerAdaptor2.play("vlc", "farzad.vlc");
+    }
 }
