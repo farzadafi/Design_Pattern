26c2fd9e67678e1a420824a988dd2ba3959dac97
farzadafi <farzadafi40@gmail.com>
2023-03-06 12:55:47 +0330

    create Mp4Player and implements from AdvanceMediaPlayer

--- Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayer.java
+++ Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayer.java
@@ -2,3 +2,16 @@ public interface AdvanceMediaPlayer {
     void loadFileName(String fileName);
     void listen();
 }
+
+class Mp4Player implements AdvanceMediaPlayer {
+
+    @Override
+    public void loadFileName(String fileName) {
+
+    }
+
+    @Override
+    public void listen() {
+
+    }
+}
