e0a7d896268d70214db76dbd79c0b9db4bd30163
farzadafi <farzadafi40@gmail.com>
2023-03-06 13:35:52 +0330

    fix play method like MediaPlayer interface

--- Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayerAdapter.java
+++ Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayerAdapter.java
@@ -1,13 +1,13 @@
 public class AdvanceMediaPlayerAdapter implements MediaPlayer{
 
-    private AdvanceMediaPlayer advanceMediaPlayer;
+    private final AdvanceMediaPlayer advanceMediaPlayer;
 
     public AdvanceMediaPlayerAdapter(AdvanceMediaPlayer advanceMediaPlayer) {
         this.advanceMediaPlayer = advanceMediaPlayer;
     }
 
     @Override
-    public void play(String fileName) {
+    public void play(String audioType, String fileName) {
         advanceMediaPlayer.loadFileName(fileName);
         advanceMediaPlayer.listen();
     }
