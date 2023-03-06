f4fdc78f763e40cb212a60e8545d3d4ef016d3ed
farzadafi <farzadafi40@gmail.com>
2023-03-06 12:56:32 +0330

    add one field and write body of two method

--- Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayer.java
+++ Adapter/D_MediaPlayer/src/main/java/AdvanceMediaPlayer.java
@@ -5,13 +5,15 @@ public interface AdvanceMediaPlayer {
 
 class Mp4Player implements AdvanceMediaPlayer {
 
+    private String fileName;
+
     @Override
     public void loadFileName(String fileName) {
-
+        this.fileName = fileName;
     }
 
     @Override
     public void listen() {
-
+        System.out.println("I listen to " + fileName);
     }
 }
