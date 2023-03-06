4707c3fac1aea6526c9e541f8cd6fb7a093cc8e1
farzadafi <farzadafi40@gmail.com>
2023-03-06 13:34:50 +0330

    fix play method and add one parameter and one condition

--- Adapter/D_MediaPlayer/src/main/java/MediaPlayer.java
+++ Adapter/D_MediaPlayer/src/main/java/MediaPlayer.java
@@ -1,11 +1,14 @@
 public interface MediaPlayer {
-    void play(String fileName);
+    void play(String audioType, String fileName);
 }
 
 class AudioPlayer implements MediaPlayer {
 
     @Override
-    public void play(String fileName) {
-        System.out.println("AudioPlayer play " + fileName);
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp3"))
+            System.out.println("AudioPlayer play " + fileName);
+        else
+            System.out.println("Invalid media " + audioType + "format not supported!");
     }
 }
