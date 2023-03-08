dbfb87e289783ea2335ec5e07ed0a72257ff85e6
farzadafi <farzadafi40@gmail.com>
2023-03-08 08:40:37 +0330

    extend from Shape class and override two method

--- Bridge/A_Shape_Color/src/main/java/shape/Circle.java
+++ Bridge/A_Shape_Color/src/main/java/shape/Circle.java
@@ -1,4 +1,21 @@
 package shape;
 
-public class Circle {
+import color.Color;
+
+public class Circle extends Shape{
+
+
+    public Circle(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void applyColor() {
+        getColor().applyColor();
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("draw " + getColor() + " Circle");
+    }
 }
