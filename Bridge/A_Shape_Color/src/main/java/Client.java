fbe68cd5e1eb0c9fb07cb2dea0e588f3d32e910e
farzadafi <farzadafi40@gmail.com>
2023-03-08 08:44:21 +0330

    add main method for test

--- Bridge/A_Shape_Color/src/main/java/Client.java
+++ Bridge/A_Shape_Color/src/main/java/Client.java
@@ -1,2 +1,20 @@
+import color.Blue;
+import color.Green;
+import shape.Circle;
+import shape.Shape;
+import shape.Square;
+
 public class Client {
+    public static void main(String[] args) {
+        Blue blue = new Blue();
+        Shape blueCircle = new Circle(blue);
+        blueCircle.applyColor();
+        blueCircle.draw();
+
+        System.out.println();
+        Green green = new Green();
+        Shape greenSquare = new Square(green);
+        greenSquare.applyColor();
+        greenSquare.draw();
+    }
 }
