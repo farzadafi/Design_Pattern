c1c7a565bcffd190139da09432e23fda5fa431cf
farzadafi <farzadafi40@gmail.com>
2023-03-08 08:39:16 +0330

    add one Color field and constructor and two other method

--- Bridge/A_Shape_Color/src/main/java/shape/Shape.java
+++ Bridge/A_Shape_Color/src/main/java/shape/Shape.java
@@ -1,4 +1,19 @@
 package shape;
 
-public class Shape {
+import color.Color;
+
+public abstract class Shape {
+
+    private final Color color;
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public abstract void applyColor();
+    public abstract void draw();
 }
