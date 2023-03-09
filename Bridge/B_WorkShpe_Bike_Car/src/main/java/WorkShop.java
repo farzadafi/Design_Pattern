870c765949acd32273bccf6b2cbd24d5698d804b
farzadafi <farzadafi40@gmail.com>
2023-03-09 20:29:39 +0330

    create Produce class and extend from WorkShop class

--- Bridge/B_WorkShpe_Bike_Car/src/main/java/WorkShop.java
+++ Bridge/B_WorkShpe_Bike_Car/src/main/java/WorkShop.java
@@ -1,3 +1,11 @@
 public abstract class WorkShop {
     abstract public void work();
 }
+
+class Produce extends WorkShop {
+
+    @Override
+    public void work() {
+        System.out.println("Produced");
+    }
+}
