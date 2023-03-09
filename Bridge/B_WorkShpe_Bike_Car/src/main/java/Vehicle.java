d2fecddbd9b0c34840eddd0fe3710c16fa32c974
farzadafi <farzadafi40@gmail.com>
2023-03-09 20:32:38 +0330

    add needed field and constructor and two getter

--- Bridge/B_WorkShpe_Bike_Car/src/main/java/Vehicle.java
+++ Bridge/B_WorkShpe_Bike_Car/src/main/java/Vehicle.java
@@ -1,2 +1,17 @@
 public abstract class Vehicle {
+    private WorkShop workShop1;
+    private WorkShop workShop2;
+
+    public Vehicle(WorkShop workShop1, WorkShop workShop2) {
+        this.workShop1 = workShop1;
+        this.workShop2 = workShop2;
+    }
+
+    public WorkShop getWorkShop1() {
+        return this.workShop1;
+    }
+
+    public WorkShop getWorkShop2() {
+        return this.workShop2;
+    }
 }
