72283796773d27da0e69bca11a765d55d631cf76
farzadafi <farzadafi40@gmail.com>
2023-03-09 20:34:35 +0330

    add Bike class and extend from Vehicle

--- Bridge/B_WorkShpe_Bike_Car/src/main/java/Vehicle.java
+++ Bridge/B_WorkShpe_Bike_Car/src/main/java/Vehicle.java
@@ -31,3 +31,17 @@ class Car extends Vehicle {
         getWorkShop2().work();
     }
 }
+
+class Bike extends Vehicle {
+
+    public Bike(WorkShop workShop1, WorkShop workShop2) {
+        super(workShop1, workShop2);
+    }
+
+    @Override
+    public void manufacture() {
+        System.out.println("Car ");
+        getWorkShop1().work();
+        getWorkShop2().work();
+    }
+}
