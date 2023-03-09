6973c0cfd44cac4a41e4163804f961ec3e8c6ee1
farzadafi <farzadafi40@gmail.com>
2023-03-09 20:51:40 +0330

    add main method for test

--- Bridge/B_WorkShpe_Bike_Car/src/main/java/Client.java
+++ Bridge/B_WorkShpe_Bike_Car/src/main/java/Client.java
@@ -1,2 +1,10 @@
 public class Client {
+    public static void main(String[] args) {
+        Vehicle carVehicle = new Car(new Produce(), new Assemble());
+        carVehicle.manufacture();
+
+        System.out.println();
+        Vehicle bikeVehicle = new Bike(new Produce(), new Assemble());
+        bikeVehicle.manufacture();
+    }
 }
