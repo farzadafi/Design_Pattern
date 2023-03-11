7edbd37a811dba18b089d718acceb4df748956d1
farzadafi <farzadafi40@gmail.com>
2023-03-11 16:43:41 +0330

    add Developer class and add needed field and method

--- Composite/A_Employee/src/main/java/Employee.java
+++ Composite/A_Employee/src/main/java/Employee.java
@@ -1,3 +1,20 @@
 public interface Employee {
     public void showEmployeeDetails();
 }
+
+class Developer implements Employee {
+    private final long id;
+    private final String name;
+    private final String position;
+
+    public Developer(long id, String name, String position) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+    }
+
+    @Override
+    public void showEmployeeDetails() {
+        System.out.println(id + " " + name + " " + position);
+    }
+}
