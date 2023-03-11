184450215d3b6105cd70d0425bbc0623c4bb143e
farzadafi <farzadafi40@gmail.com>
2023-03-11 16:44:15 +0330

    add Manager class and add needed field and method

--- Composite/A_Employee/src/main/java/Employee.java
+++ Composite/A_Employee/src/main/java/Employee.java
@@ -18,3 +18,20 @@ class Developer implements Employee {
         System.out.println(id + " " + name + " " + position);
     }
 }
+
+class Manager implements Employee {
+    private final long id;
+    private final String name;
+    private final String position;
+
+    public Manager(long id, String name, String position) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+    }
+
+    @Override
+    public void showEmployeeDetails() {
+        System.out.println(id + " " + name + " " + position);
+    }
+}
