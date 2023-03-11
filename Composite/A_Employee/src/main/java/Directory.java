f47b23efa28860d281273d986674fd0c65fc1bbd
farzadafi <farzadafi40@gmail.com>
2023-03-11 16:46:50 +0330

    add a list and override showEmployeeDetails method

--- Composite/A_Employee/src/main/java/Directory.java
+++ Composite/A_Employee/src/main/java/Directory.java
@@ -1,2 +1,23 @@
-public class Directory {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements Employee {
+
+    private final List<Employee> employees = new ArrayList<>();
+
+    @Override
+    public void showEmployeeDetails() {
+        for (Employee e : employees
+                ) {
+            e.showEmployeeDetails();
+        }
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee);
+    }
 }
