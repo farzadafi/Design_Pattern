70570c59697ba32bc70556197e4e05bb9d89cf16
farzadafi <farzadafi40@gmail.com>
2023-03-12 14:14:50 +0330

    add Professor class and add nedded field

--- Composite/C_College/src/main/java/Faculty.java
+++ Composite/C_College/src/main/java/Faculty.java
@@ -1,3 +1,20 @@
 public interface Faculty {
     String getDetails();
 }
+
+class Professor implements Faculty {
+    private int id;
+    private final String name;
+    private final String position;
+
+    public Professor(int id, String name, String position) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+    }
+
+    @Override
+    public String getDetails() {
+        return (name +  " is the " + position);
+    }
+}
