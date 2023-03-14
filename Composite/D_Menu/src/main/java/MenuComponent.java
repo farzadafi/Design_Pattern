16e431c587bcb6a9f678b315a398126165cae058
farzadafi <farzadafi40@gmail.com>
2023-03-14 08:58:33 +0330

    add two field and one list and some needed method

--- Composite/D_Menu/src/main/java/MenuComponent.java
+++ Composite/D_Menu/src/main/java/MenuComponent.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MenuComponent {
+    private final String url;
+    private final String name;
+    private List<MenuComponent> children;
+
+    public MenuComponent(String url, String name) {
+        this.url = url;
+        this.name = name;
+        children = new ArrayList<>();
+    }
+
+    public MenuComponent add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("not supported in this level");
+    }
+
+    public MenuComponent remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("not supported in this level");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuComponent that = (MenuComponent) o;
+        return Objects.equals(url, that.url) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, name);
+    }
 }
