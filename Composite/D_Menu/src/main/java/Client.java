94b9feeda03e04b058697a4fa9e3e7010f62f048
farzadafi <farzadafi40@gmail.com>
2023-03-14 09:28:33 +0330

    add main method for test

--- Composite/D_Menu/src/main/java/Client.java
+++ Composite/D_Menu/src/main/java/Client.java
@@ -1,2 +1,14 @@
 public class Client {
+    public static void main(String[] args) {
+        Menu root = new Menu("//transfer", "Transfer Money");
+        root.add(new MenuItem("//card to card", "card to card"));
+        root.add(new MenuItem("//account", "By Account number"));
+
+        Menu subMenu = new Menu("//by Iban", "by Iban");
+        subMenu.add(new MenuItem("//paya", "paya"));
+        subMenu.add(new MenuItem("//satna", "satna"));
+
+        root.add(subMenu);
+        System.out.println(root);
+    }
 }
