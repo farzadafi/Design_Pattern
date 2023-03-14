8385c45bd3927bb9865b66289138b6003ebe0da5
farzadafi <farzadafi40@gmail.com>
2023-03-14 09:01:54 +0330

    create Menu class and extend form MenuComponent class

---
+++ Composite/D_Menu/src/main/java/Menu.java
@@ -0,0 +1,11 @@
+public class Menu extends MenuComponent {
+
+    public Menu(String url, String name) {
+        super(url, name);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
