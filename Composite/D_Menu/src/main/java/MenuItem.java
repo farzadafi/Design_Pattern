0c9740e40eac6198bf3598160299dbd810f1df99
farzadafi <farzadafi40@gmail.com>
2023-03-14 09:11:45 +0330

    extend from MenuComponent and override toString method

--- Composite/D_Menu/src/main/java/MenuItem.java
+++ Composite/D_Menu/src/main/java/MenuItem.java
@@ -1,2 +1,11 @@
-public class MenuItem {
+public class MenuItem extends MenuComponent {
+
+    public MenuItem(String url, String name) {
+        super(url, name);
+    }
+
+    @Override
+    public String toString() {
+        return "::item:: " + print();
+    }
 }
