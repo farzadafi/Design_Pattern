b32ec27fb5b127b9696d5a9f5b92a67ed7e1d932
farzadafi <farzadafi40@gmail.com>
2023-04-19 06:32:31 +0330

    add ConcreteDecoratorEx_2 class

--- Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
+++ Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
@@ -19,3 +19,12 @@ class ConcreteDecoratorEx_1 extends AbstractDecorator {
         System.out.println("I am explicitly from Ex_1");
     }
 }
+
+class ConcreteDecoratorEx_2 extends AbstractDecorator {
+    public void doJob() {
+        super.doJob();
+
+        // add additional responsibilities
+        System.out.println("I am explicitly from Ex_2");
+    }
+}
