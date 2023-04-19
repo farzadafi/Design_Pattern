b6a1a2fdd4d99e0326901e8c4b21ea41ca988fb4
farzadafi <farzadafi40@gmail.com>
2023-04-19 06:30:37 +0330

    add doJob method

--- Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
+++ Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
@@ -1,2 +1,12 @@
-public class AbstractDecorator {
+abstract public class AbstractDecorator {
+    protected Component component;
+
+    public void setComponent(Component component) {
+        this.component = component;
+    }
+
+    public void doJob() {
+        if (component != null)
+                component.doJob();
+    }
 }
