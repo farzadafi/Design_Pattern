30bfdd51747646e25138246b2b0d6020775a4815
farzadafi <farzadafi40@gmail.com>
2023-04-19 07:10:26 +0330

    add main method for test

--- Decorator/A_Component_DoJob/src/main/java/Client.java
+++ Decorator/A_Component_DoJob/src/main/java/Client.java
@@ -1,2 +1,15 @@
 public class Client {
+    public static void main(String[] args) {
+        System.out.println("Decorator Pattern Demo");
+
+        ConcreteComponent concreteComponent = new ConcreteComponent();
+        ConcreteDecoratorEx_1 ex1 = new ConcreteDecoratorEx_1();
+        ex1.setComponent(concreteComponent);
+        ex1.doJob();
+
+        System.out.println();
+        ConcreteDecoratorEx_2 ex2 = new ConcreteDecoratorEx_2();
+        ex2.setComponent(ex1);
+        ex2.doJob();
+    }
 }
