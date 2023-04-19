8b21adc5d13d0a6c9b066e5719321d24e04c7764
farzadafi <farzadafi40@gmail.com>
2023-04-19 07:17:34 +0330

    add to read method

--- Decorator/B_IO_Example/src/main/java/LowerCaseInputStream.java
+++ Decorator/B_IO_Example/src/main/java/LowerCaseInputStream.java
@@ -1,2 +1,24 @@
-public class LowerCaseInputStream {
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowerCaseInputStream extends FilterInputStream {
+
+    public LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+    
+    public int read() throws IOException {
+        int c = in.read();        
+        return ( c == -1 ? c : Character.toLowerCase( (char) c));
+    }
+    
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result  =in.read(b, offset, len);
+
+        for (int i = 0; i < offset + result; i++)
+                b[i] = (byte) Character.toLowerCase( (char) b[i]);
+
+        return result;
+    }
 }
