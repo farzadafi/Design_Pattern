8e3fc75a132648c6317820b8f6f0516d4e787437
farzadafi <farzadafi40@gmail.com>
2023-04-19 06:37:42 +0330

    extend AbstractDecorator from Component

--- Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
+++ Decorator/A_Component_DoJob/src/main/java/AbstractDecorator.java
@@ -1,10 +1,11 @@
-abstract public class AbstractDecorator {
+abstract public class AbstractDecorator extends Component {
     protected Component component;
 
     public void setComponent(Component component) {
         this.component = component;
     }
 
+    @Override
     public void doJob() {
         if (component != null)
                 component.doJob();
