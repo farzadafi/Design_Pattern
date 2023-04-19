fc5cb234a577880123a959647b53fdeb0adffb60
farzadafi <farzadafi40@gmail.com>
2023-04-19 06:28:13 +0330

    create ConcreteComponent class and extends form Component class

--- Decorator/A_Component_DoJob/src/main/java/Component.java
+++ Decorator/A_Component_DoJob/src/main/java/Component.java
@@ -1,3 +1,11 @@
 abstract public class Component {
     public abstract void doJob();
 }
+
+class ConcreteComponent extends Component {
+
+    @Override
+    public void doJob() {
+        System.out.println("I am a Concrete Component");
+    }
+}
