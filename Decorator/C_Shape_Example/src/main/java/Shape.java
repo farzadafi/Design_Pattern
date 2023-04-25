e05ea792094dd8093c2d507e3e9dcecec211d625
farzadafi <farzadafi40@gmail.com>
2023-04-25 12:55:50 +0330

    create Circle and implements from Shape interface

--- Decorator/C_Shape_Example/src/main/java/Shape.java
+++ Decorator/C_Shape_Example/src/main/java/Shape.java
@@ -10,3 +10,11 @@ class Rectangle implements Shape {
         System.out.println("Shape: Rectangle");
     }
 }
+
+class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
