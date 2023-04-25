b29a5ffe8b98ef22116fc1660a00bfd36f9e3ab7
farzadafi <farzadafi40@gmail.com>
2023-04-25 13:12:49 +0330

    create RedShapeDecorator and extends from ShapeDecorator

--- Decorator/C_Shape_Example/src/main/java/ShapeDecorator.java
+++ Decorator/C_Shape_Example/src/main/java/ShapeDecorator.java
@@ -10,3 +10,20 @@ public abstract class ShapeDecorator implements Shape {
         decoratedShape.draw();
     }
 }
+
+class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape) {
+        System.out.println("Border Color: Red");
+    }
+}
