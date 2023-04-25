373b8bc198cb0a9b40783509984ff9f8a18e9392
farzadafi <farzadafi40@gmail.com>
2023-04-25 14:06:11 +0330

    add compress method

--- Decorator/D_CloudStream_Mosh/src/main/java/CompressedCloudStream.java
+++ Decorator/D_CloudStream_Mosh/src/main/java/CompressedCloudStream.java
@@ -8,7 +8,11 @@ public class CompressedCloudStream implements Stream {
 
     @Override
     public void write(String data) {
-
+        String compress = compress(data);
+        stream.write(compress);
     }
 
+    private String compress(String data) {
+        return data.substring(0, 5);
+    }
 }
