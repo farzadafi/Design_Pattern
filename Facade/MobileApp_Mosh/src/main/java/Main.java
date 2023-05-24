a61158580273904d79a7c2132f460bb52cebc090
farzadafi <farzadafi40@gmail.com>
2023-05-24 09:33:08 +0330

    fix main method and write code with and without facade pattern

--- Facade/MobileApp_Mosh/src/main/java/Main.java
+++ Facade/MobileApp_Mosh/src/main/java/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        //without Facade :(
         var server = new NotificationServer();
         var connection = server.connection("id");
         Message message = new Message("Hello world");
         AuthToken authToken = server.authenticate("appId", "key");
         server.send(authToken, message, "target");
         connection.disconnect();
+
+        //with Facade :)
+        NotificationService notificationService = new NotificationService();
+        notificationService.send("Hello World", "farzadafi");
     }
 }
