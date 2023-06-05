f3225a8eb1dd2791b6d9bb8f905a9e7d33aa8742
farzadafi <farzadafi40@gmail.com>
2023-06-05 18:53:15 +0330

    create WordExtractorReport

---
+++ Abstract_Class/src/main/java/WordExtractorReport.java
@@ -0,0 +1,18 @@
+import java.util.regex.Pattern;
+
+public class WordExtractorReport extends ExtractorReport {
+
+    private static final Pattern PATTERN =
+            Pattern.compile("[A-Za-z]+", Pattern.CASE_INSENSITIVE);
+
+
+    @Override
+    public Pattern getPattern() {
+        return PATTERN;
+    }
+
+    @Override
+    public String getReportName() {
+        return "Word";
+    }
+}
