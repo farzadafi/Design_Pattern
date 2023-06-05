105690faa9352ea8e0d99a09d66413d8215ea5b1
farzadafi <farzadafi40@gmail.com>
2023-06-05 18:12:51 +0330

    create ExtractorReport and add prepareAndSendReport method

---
+++ Abstract_Class/A_one_method_approach/src/main/java/ExtractorReport.java
@@ -0,0 +1,35 @@
+package main.java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractorReport {
+    
+    public static String prepareAndSendReport(Pattern pattern, String reportName, String path) throws FileNotFoundException {
+        StringBuilder out = new StringBuilder();
+        URL url = ExtractorReport.class.getResource(path);
+        assert url != null;
+        File file = new File(url.getPath());
+        Scanner scanner = new Scanner(file);
+        if (scanner.hasNext())
+            scanner.nextLine();
+        else
+            return "Empty file";
+        while (scanner.hasNext()) {
+            String nextLine = scanner.nextLine();
+            Matcher matcher = pattern.matcher(nextLine);
+            boolean matches = matcher.matches();
+            if (matches) {
+                out.append(nextLine).append("\n");
+            }
+        }
+        String report = out.length() == 0 ? "Empty file" : out.toString();
+        System.out.println("Starting report " + reportName);
+        System.out.println(report);
+        return report;
+    }
+}
