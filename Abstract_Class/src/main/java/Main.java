6cc3f5a43b852516c13b58e1101ea74d772e69e6
farzadafi <farzadafi40@gmail.com>
2023-06-05 18:53:27 +0330

    add new tet for WordExtractorReport class

--- Abstract_Class/src/main/java/Main.java
+++ Abstract_Class/src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         new NumberExtractorReport().prepareAndSendReport("data.txt");
         new EmailExtractorReport().prepareAndSendReport("data.txt");
+        new WordExtractorReport().prepareAndSendReport("data.txt");
     }
 }
