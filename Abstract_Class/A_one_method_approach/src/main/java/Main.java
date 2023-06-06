5335ed82b5dbb68cc76f3c493d4841a1dfe97a51
farzadafi <farzadafi40@gmail.com>
2023-06-06 16:26:59 +0330

    fix main test all report

--- Abstract_Class/A_one_method_approach/src/main/java/Main.java
+++ Abstract_Class/A_one_method_approach/src/main/java/Main.java
@@ -8,12 +8,12 @@ public class Main {
         String path = "/main/resources/data.txt";
 
         Pattern numberPattern = Pattern.compile("\\d+");
-        String numberReport = ExtractorReport.prepareAndSendReport(numberPattern, "Number", path);
+        ExtractorReport.prepareAndSendReport(numberPattern, "Number", path);
 
         Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}");
-        String emailReport = ExtractorReport.prepareAndSendReport(emailPattern, "Email", path);
+        ExtractorReport.prepareAndSendReport(emailPattern, "Email", path);
 
         Pattern wordPattern = Pattern.compile("[A-Za-z]+");
-        String wordReport = ExtractorReport.prepareAndSendReport(wordPattern, "Word", path);
+        ExtractorReport.prepareAndSendReport(wordPattern, "Word", path);
     }
 }
