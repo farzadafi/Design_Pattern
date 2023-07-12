ab0a96067205c6ba520cb39523d364ac6794bfb2
farzadafi <farzadafi40@gmail.com>
2023-07-12 13:52:05 +0330

    create Main class and add main method

---
+++ Factory_Method/view_engine/src/main/java/Main.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductController productController = new ProductController();
+        productController.listProduct();
+    }
+}
