59693acb4bedc774957d11eb5619705bb1698ddc
farzadafi <farzadafi40@gmail.com>
2023-07-12 13:51:30 +0330

    change name to GlassViewEngine class

---
+++ Factory_Method/view_engine/src/main/java/GlassViewEngine.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class GlassViewEngine {
+    public String render(String viewName) {
+        return viewName + " View file rendered with glass view engine";
+    }
+}
--- Factory_Method/view_engine/src/main/java/MatchaViewEngine.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class MatchaViewEngine {
-    public String render(String viewName) {
-        return "View rendered with Matcha";
-    }
-}
