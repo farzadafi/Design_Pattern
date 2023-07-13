b4a125cea4f27680ce56fd24eba7849bd6bd8b13
farzadafi <farzadafi40@gmail.com>
2023-07-13 14:30:19 +0330

    fix render method implementation

--- Factory_Method/view_engine/view_engine_2/src/main/java/GlassViewEngine.java
+++ Factory_Method/view_engine/view_engine_2/src/main/java/GlassViewEngine.java
@@ -1,9 +1,31 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GlassViewEngine implements ViewEngine {
     @Override
-    public String render(String viewName) {
-        return viewName + " View file rendered with glass view engine";
+    public String render(String path) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return path + " View file rendered with glass view engine";
     }
 
 }
