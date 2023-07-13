6b37ed8fe463d66f1d728cf808b9bd54d8a1f379
farzadafi <farzadafi40@gmail.com>
2023-07-13 14:29:49 +0330

    fix render method

--- Factory_Method/view_engine/view_engine_2/src/main/java/PageRender.java
+++ Factory_Method/view_engine/view_engine_2/src/main/java/PageRender.java
@@ -1,8 +1,8 @@
 package main.java;
 
 public class PageRender {
-    public void render(String viewName, ViewEngine viewEngine) {
-        String renderMatcha = viewEngine.render(viewName);
-        System.out.println(renderMatcha);
+    public void render(String path, ViewEngine viewEngine) {
+        String pageRenderStr = viewEngine.render(path);
+        System.out.println(pageRenderStr);
     }
 }
