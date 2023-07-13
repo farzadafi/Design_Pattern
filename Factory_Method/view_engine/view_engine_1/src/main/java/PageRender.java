9717c62b2f9a5a5769e15e16e1911b52941a3b0c
farzadafi <farzadafi40@gmail.com>
2023-07-13 14:22:28 +0330

    fix render method and change field string name

--- Factory_Method/view_engine/view_engine_1/src/main/java/PageRender.java
+++ Factory_Method/view_engine/view_engine_1/src/main/java/PageRender.java
@@ -3,7 +3,7 @@ package main.java;
 public class PageRender {
     public void render(String path) {
         GlassViewEngine glassViewEngine = new GlassViewEngine();
-        String renderMatcha = glassViewEngine.render(path);
-        System.out.println(renderMatcha);
+        String pageRenderStr = glassViewEngine.render(path);
+        System.out.println(pageRenderStr);
     }
 }
