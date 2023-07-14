b53922cb0cdbc1dc4bfea39ad8fa42ec98b6259b
farzadafi <farzadafi40@gmail.com>
2023-07-14 07:39:47 +0330

    add MugViewEngine class

---
+++ Factory_Method/view_engine/view_engine_2/src/main/java/MugViewEngine.java
@@ -0,0 +1,20 @@
+package main.java;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class MugViewEngine implements ViewEngine {
+    @Override
+    public String render(String path) {
+        Path pathFile = Paths.get(path);
+        try (Stream<String> lines = Files.lines(pathFile)) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path + " View file rendered with mug view engine";
+    }
+}
