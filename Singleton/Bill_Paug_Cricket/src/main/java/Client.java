72ab7271187fb95ad3766ea9092e36e7821492f3
farzadafi <farzadafi40@gmail.com>
2023-07-23 06:54:04 +0330

    remove Bill_Pugh_Cricket example in singelton module

--- Singleton/Bill_Paug_Cricket/src/main/java/Client.java
+++
@@ -1,8 +0,0 @@
-public class Client {
-    public static void main(String[] args) {
-        Captain captain = Captain.getInstance();
-        System.out.println(captain);
-        Captain captain1 = Captain.getInstance();
-        System.out.println(captain1);
-    }
-}
