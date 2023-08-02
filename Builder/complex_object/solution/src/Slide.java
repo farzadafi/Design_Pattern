e67684e42e1764aa835ff51da0d08cfbedc708a1
farzadafi <farzadafi40@gmail.com>
2023-08-02 16:16:21 +0330

    add Slide class

---
+++ Builder/complex_object/solution/src/Slide.java
@@ -0,0 +1,11 @@
+public class Slide {
+    private final String text;
+
+    public Slide(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
