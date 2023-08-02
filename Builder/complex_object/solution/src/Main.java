4d6783d06e34cbb5506ce6f8898089c6b37338f5
farzadafi <farzadafi40@gmail.com>
2023-08-02 16:19:12 +0330

    add Main class for test

---
+++ Builder/complex_object/solution/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        var presentation = new Presentation();
+        presentation.addSlide(new Slide("Slide 1"));
+        presentation.addSlide(new Slide("Slide 2"));
+
+        var builder = new PdfDocumentBuilder();
+        presentation.export(builder);
+        var pdf = builder.getDocument();
+
+        System.out.println();
+
+        var builderMovie = new MovieBuilder();
+        presentation.export(builderMovie);
+        var movie = builderMovie.getMovie();
+    }
+}
