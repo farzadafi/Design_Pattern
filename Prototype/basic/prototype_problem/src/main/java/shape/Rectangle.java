517902eacfe3f322c24b6ca3baeefa05ad71a6a1
farzadafi <farzadafi40@gmail.com>
2023-08-15 12:57:03 +0330

    create Rectangle class with 2 attribute

---
+++ Prototype/basic/prototype_problem/src/main/java/shape/Rectangle.java
@@ -0,0 +1,32 @@
+package main.java.shape;
+
+public class Rectangle implements Component {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Render a Rectangle");
+    }
+}
