4734b9adb5c84f42c4946f58a5fa140f5f934c1b
farzadafi <farzadafi40@gmail.com>
2023-08-15 14:12:49 +0330

    create ContextMenu class and add duplicate method

---
+++ Prototype/basic/prototype_solution/src/main/java/ContextMenu.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import main.java.shape.Component;
+
+public class ContextMenu {
+    public Component duplicate(Component component) {
+        return component.clone();
+    }
+}
