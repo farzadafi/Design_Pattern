e7aa729fc0c96c128ca9b766645f86d563bd724f
farzadafi <farzadafi40@gmail.com>
2023-08-15 14:13:06 +0330

    create Main class and add main method for test

---
+++ Prototype/basic/prototype_solution/src/main/java/Main.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import main.java.shape.Circle;
+import main.java.shape.Component;
+import main.java.shape.Rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        Circle circle = new Circle();
+        circle.setRadius(10);
+        Component duplicate = contextMenu.duplicate(circle);
+        Circle circle1 = (Circle) duplicate;
+        System.out.println(circle1.getRadius());
+        circle1.render();
+
+        Rectangle rectangle = new Rectangle(20, 30);
+        duplicate = contextMenu.duplicate(rectangle);
+        Rectangle rectangle1 = (Rectangle) duplicate;
+        System.out.println("\n" + rectangle1.getLength());
+        System.out.println(rectangle1.getWidth());
+    }
+}
