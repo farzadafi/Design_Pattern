1b2a0f7e589b4272b0c919229ba51f54ba434edd
farzadafi <farzadafi40@gmail.com>
2023-08-15 12:57:29 +0330

    move under shape package

--- Prototype/basic/prototype_problem/src/main/java/shape/Circle.java
+++ Prototype/basic/prototype_problem/src/main/java/shape/Circle.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.shape;
 
 public class Circle implements Component{
     private int radius;
--- Prototype/basic/prototype_problem/src/main/java/shape/Component.java
+++ Prototype/basic/prototype_problem/src/main/java/shape/Component.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.shape;
 
 public interface Component {
     void render();
