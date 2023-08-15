ac561e9f8ec262ca8fc9e36a6e2c68449579b650
farzadafi <farzadafi40@gmail.com>
2023-08-15 13:25:14 +0330

    fix duplidate method

--- Prototype/basic/prototype_problem/src/main/java/ContextMenu.java
+++ Prototype/basic/prototype_problem/src/main/java/ContextMenu.java
@@ -1,11 +1,20 @@
 package main.java;
 
+import main.java.shape.Circle;
+import main.java.shape.Component;
+import main.java.shape.Rectangle;
+
+// this class a menu that has some utility methods, for example duplicate, copy, paste or ...
 public class ContextMenu {
-    public void duplicate(Component component) {
-        if( component instanceof Circle){
-            Circle source = (Circle) component;
+    public Component duplicate(Component component) {
+        if (component instanceof Circle source) { //if is true, create a Circle with name source, and use with that :)
             Circle target = new Circle();
             target.setRadius(source.getRadius());
+            component = target;
         }
+        if (component instanceof Rectangle source) {
+            component = new Rectangle(source.getLength(), source.getWidth());
+        }
+        return component;
     }
 }
