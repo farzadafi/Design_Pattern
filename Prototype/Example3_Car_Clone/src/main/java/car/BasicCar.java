1e7b1165947d81efa9e74c518a5e069390c96a05
farzadafi <farzadafi40@gmail.com>
2023-08-16 09:20:57 +0330

    create separate BasicCar class

---
+++ Prototype/Example3_Car_Clone/src/main/java/car/BasicCar.java
@@ -0,0 +1,20 @@
+package car;
+
+public class BasicCar implements Cloneable {
+    private String modelName;
+    private Long price;
+
+    public BasicCar(String modelName, Long price) {
+        this.modelName = modelName;
+        this.price = price;
+    }
+
+    @Override
+    public BasicCar clone() {
+        try {
+            return (BasicCar) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+}
